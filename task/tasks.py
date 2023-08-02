6ea6bb3a27c754d3f3b83d22d7cbfcbd11042f88
dapangyu <zhaoyihuan@bilibili.com>
2023-08-02 11:14:58 +0800

    完善大致步骤

--- task/tasks.py
+++ task/tasks.py
@@ -1,23 +1,45 @@
 from __future__ import absolute_import
 
+from subprocess import run, Popen, PIPE
 from task.celery import app
+
+DUPLICATE_BASE = '/root/repo/tmp/duplicate'
 
 
 @app.task
-def create_a_duplicate(path):
-    pass
+def create_a_duplicate(path: str):
+    repo_path = '/root/repo/github/{0}'.format(path)
+    duplicate_path = '{0}/{1}'.format(DUPLICATE_BASE, path)
+    args = ['mkdir', '-p', duplicate_path]
+    result = run(args, check=True, capture_output=True)
+    print(result)
+    args = ['rsync', '-aP', repo_path, duplicate_path]
+    result = run(args, check=True, capture_output=True)
+    print(result)
+    return result
 
 
 @app.task
 def delete_a_duplicate(path):
-    pass
+    duplicate_path = '{0}/{1}'.format(DUPLICATE_BASE, path)
+    args = ['rm', '-rf', duplicate_path]
+    result = run(args, check=True, capture_output=True)
+    print(result)
+    return result
 
 
 @app.task
-def delete_all_duplicate(path):
-    pass
+def delete_all_duplicate():
+    args = ['rm', '-rf', DUPLICATE_BASE]
+    result = run(args, check=True, capture_output=True)
+    print(result)
+    return result
 
 
 @app.task
 def updating_duplicated_repo(path):
-    pass
+    duplicate_path = '{0}/{1}'.format(DUPLICATE_BASE, path)
+    args = ['/usr/bin/sync_repo']
+    result = run(args, check=True, cwd=duplicate_path)
+    print(result)
+    return result
