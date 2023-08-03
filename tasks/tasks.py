0b708891f00b216d9e5c40f9fa4b6b7283b96cfd
dapangyu-fish <2501808198@qq.com>
2023-08-03 14:35:38 +0800

    path错误

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -11,8 +11,8 @@ DUPLICATE_BASE = '/root/repo/tmp/duplicate'
 
 @app.task
 def create_a_duplicate(path: str):
-    repo_path = '/root/repo/github.com/{0}'.format(path)
-    duplicate_father_path = '{0}/github.com/{1}'.format(DUPLICATE_BASE, os.path.dirname(path))
+    repo_path = '/root/repo/{0}'.format(path)
+    duplicate_father_path = '{0}/{1}'.format(DUPLICATE_BASE, os.path.dirname(path))
     args = ['mkdir', '-p', duplicate_father_path]
     result = run(args, check=True, capture_output=True)
     print(result)
@@ -29,7 +29,7 @@ def create_a_duplicate(path: str):
 
 @app.task
 def delete_a_duplicate(path):
-    duplicate_path = '{0}/github.com/{1}'.format(DUPLICATE_BASE, path)
+    duplicate_path = '{0}/{1}'.format(DUPLICATE_BASE, path)
     args = ['rm', '-rf', duplicate_path]
     result = run(args, check=True, capture_output=True)
     data = {
@@ -56,7 +56,7 @@ def delete_all_duplicate():
 
 @app.task
 def updating_duplicated_repo(path):
-    duplicate_path = '{0}/github.com/{1}'.format(DUPLICATE_BASE, path)
+    duplicate_path = '{0}/{1}'.format(DUPLICATE_BASE, path)
     args = ['/usr/bin/sync_repo']
     result = run(args, check=True, cwd=duplicate_path)
     data = {
