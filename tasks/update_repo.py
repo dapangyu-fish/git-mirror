866d80dca7365f87d212c7fb780e80efbee9d770
dapangyu-fish <2501808198@qq.com>
2023-08-04 16:47:34 +0800

    test 1 repo

--- tasks/update_repo.py
+++ tasks/update_repo.py
@@ -1,5 +1,21 @@
 from service.redis_test import RedisShark, redis_obj, RepoStatus
-from tasks.tasks import create_a_duplicate
+from tasks.tasks import create_a_duplicate,updating_duplicated_repo,update_repo
 
 
-create_a_duplicate.delay("github.com/moby/moby.git")
+def update_repo(repo_path):
+    step1 = create_a_duplicate.delay(test_repo)
+    r = step1.get()
+    print("step1 result:{0}".format(r))
+
+    step2 = updating_duplicated_repo.delay(test_repo)
+    r = step2.get()
+    print("step2 result:{0}".format(r))
+
+    step3 = update_repo.delay(test_repo)
+    r = step3.get()
+    print("step3 result:{0}".format(r))
+
+
+if __name__ == '__main__':
+    test_repo = "github.com/moby/moby.git"
+    create_a_duplicate.delay(test_repo)
